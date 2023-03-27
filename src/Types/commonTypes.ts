ac0f45d51efa65503f27103981b2ca6cc05e2f2f
uchenick55 <uchenick55@yandex.ru>
2023-03-27 20:20:25 +0300

    добавил функцию fire в редьюсер

---
+++ src/Types/commonTypes.ts
@@ -0,0 +1,4 @@
+export type ShipType = {
+    locations: Array<string> ,// ["06", "16", "26"]
+    hits: Array< "" | "hit" > // ["", "hit", ""]
+}
--- src/redux/data-reducer.ts
+++ src/redux/data-reducer.ts
@@ -1,35 +1,61 @@
 
-/*
-const SET_AAA = "myApp/bestChangeReducer/SET_BEST_CHANGE_DATA"; //константа задания данных с сервера
-*/
+import {ShipType} from "../Types/commonTypes";
 
+type fireType = (
+    uess: any,
+    ships:Array<ShipType>,
+    isSunk: (ship:ShipType)=>boolean,
+    shipsSunk: number
+) => boolean
 
-/*
-type setBestChangeDataActionType = { type: typeof SET_BEST_CHANGE_DATA, bestChangeData: Array<PairType> | null }
-export const setBestChangeData = (bestChangeData: Array<PairType> | null): setBestChangeDataActionType => { // экшн задания данных с сервера
-    return {type: SET_BEST_CHANGE_DATA, bestChangeData}
-};
-*/
+const fire:fireType = (guess, ships, isSunk, shipsSunk) => { // проверка попадания по выбранному полю
+    for (var i = 0; i < ships.length; i++) { // пробегаем массив ships
+        var ship = ships[i]; // получить данные по текущему кораблю
+        var locationIndex = ship.locations.indexOf(guess); // поиск guess в массиве позиций кораблей
+        if (locationIndex >= 0) { //    если совпадает с одним из полей и попадание не повторно
+            if ( ship.hits[locationIndex] === "hit") { // если эта часть корабля уже была потоплена раньше
+                // view.displayMessage("Эта часть корабля уже была потоплена")
+                return false // прервать выполнение функции
+            }
+            ship.hits[locationIndex] = "hit"; // присвоить соответствующему полю hits
+            //  view.displayHit(guess)// отобразить попадания в корабли
+            //  view.displayMessage("HIT")
+            if (isSunk(ship)) { // если корабль потоплен (вернет true)
+                //    ships[i].isSunk=true
+                //    view.displayMessage("You sank my battleship!")
+                shipsSunk++; // увеличиваем счетчик потопленых корабелей на 1
+            }
+            return true // вернуть подтвержение попадание
+        }
+    }
 
-type ActionTypes = any
-
-type ShipType = {
-    locations: Array<string> ,//["00", "00", "00"]
-    hits: Array<string> // ["", "", ""]
+    // view.displayMiss(guess)// отобразить промахи мимо кораблей
+    //view.displayMessage("MISS")
+    return false // если условия не выполнились, попадания не было (MISS)
 }
+
+const FIRE = "sbrrt/dataReducer/FIRE"; //константа выстрела
+
+type fireActionType = { type: typeof FIRE, guess: string}
+export const setFire = (guess: string): fireActionType => { // экшн получения данных выстрела
+    return {type: FIRE, guess}
+};
+
+type ActionTypes = fireActionType
 
 type initialStateType = {
-    boardSize: number,// размер клеток поля (пока заглушка)
-    numShips: number, // количество кораблей (пока заглушка)
-    shipLength: number, //длина кораблей (пока заглушка)
+    boardSize: number,// размер клеток поля
+    numShips: number, // количество кораблей
+    shipLength: number, //длина кораблей
     shipsSunk: number, // сколько кораблей уже потоплено
-    ships:Array<ShipType>
+    ships:Array<ShipType>,
+
 }
 const initialState: initialStateType = { //стейт по умолчанию
-    boardSize:7,
-    numShips: 3,
-    shipLength: 3,
-    shipsSunk:0,
+    boardSize:7,// размер клеток поля
+    numShips: 3,// количество кораблей
+    shipLength: 3,//длина кораблей
+    shipsSunk:0, // сколько кораблей уже потоплено
     ships: [ // данные по кораблям (положение на поле и массивы попаданий)
         { locations: ["06", "16", "26"], hits: ["", "", ""] },
         { locations: ["24", "34", "44"], hits: ["", "", ""] },
@@ -40,40 +66,16 @@ const initialState: initialStateType = { //стейт по умолчанию
 const dataReducer = (state: initialStateType = initialState, action: ActionTypes): initialStateType => {//редьюсер
     let stateCopy: initialStateType; // объявлениечасти части стейта до изменения редьюсером
     switch (action.type) {
-/*        case SET_AAA: // кейс задания ошибок формы
+        case FIRE:  // кейс задания ошибок формы
+
             stateCopy = {
                 ...state, // копия всего стейта
             }
-            return stateCopy; // возврат копии стейта после изменения*/
+            return stateCopy; // возврат копии стейта после изменения
         default:
             return state; // по умолчанию стейт возврашается неизмененным
     }
 }
-
-/*
-export const getBestChangeDataTC //санкреатор получения данных из внешнего источника
-    = (): ThunkAction<
-        Promise<void>, // санка ничего не возвращает, void
-        GlobalStateType, // тип глобального стейта из redux-store
-        unknown, // доп аргументы
-        ActionTypes // возможные типы экшенов этого редьюсера
-        > => {
-    return async (dispatch) => { // санка получения данных из внешнего источника
-        const response2 = await api.getBestChangeData()  //получить данные из внешнего источника
-        if (response2) {// если они не пустые
-            dispatch({type: SET_BEST_CHANGE_DATA, bestChangeData:response2})  //записать считаное из localStorage значение темы в store
-        }
-    }
-}
-
-export const getBestChangeDataTC1 = () => {//санкреатор получения данных из внешнего источника
-    return async (dispatch: Dispatch<ActionTypes>, getState: () => GlobalStateType) => { // санка получения данных из localStorage
-        const response2 = await apiCommon.getData()
-        dispatch(response2)  //записать считаное из localStorage значение темы в store
-    }
-}
-*/
-
 
 export default dataReducer;
 
